6ca5a00bccca5166b3d412eeb62d9cb75a9a75b8
KevinDombeu <dombeukevine@gmail.com>
2025-05-04 21:12:14 +0100

    Premier commit : routes Ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AccueilComponent },
+    { path: 'accueil', component: AccueilComponent },
+    { path: 'user-list', component: UserListComponent },
+    { path: '**', redirectTo: '/user-list', pathMatch: 'full' }
+];
